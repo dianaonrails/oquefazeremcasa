5108fd70cad2fe1ad2ac349e8c96b6e9b45e2f14
dianaonrails <d.r.carvalho89@gmail.com>
2020-03-26 23:45:38 +0000

    escrever

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var coisas = Array("Ler um livro", "Pintar", "Videojogos", "Arrumar o armário", "Limpar a casa", "Fazer exercício","Fazer um puzzle","Tomar um banho quentinho",
-	"Fazer panquecas","Aprender uma língua nova","Dançar","Ver um filme","Jogos de tabuleiro", "Meditar", "Yoga", "Pilates", "Jogo do Stop", "Quizzes", "Experimentar receita nova", "Reorganizar os móveis", "Bricolage", "Jogar ao macaquinho do chinês", "Fazer pão", "Fazer um bolo", "Desenhar", "Visitar um museu online");
+	"Fazer panquecas","Aprender uma língua nova","Dançar","Ver um filme","Jogos de tabuleiro", "Meditar", "Yoga", "Pilates", "Jogo do Stop", "Quizzes", "Experimentar receita nova", "Reorganizar os móveis", "Bricolage", "Jogar ao macaquinho do chinês", "Fazer pão", "Fazer um bolo", "Desenhar", "Visitar um museu online", "Escrever");
 
 function randomCoisas() {
   var randomCoisas = coisas[Math.floor(Math.random() * coisas.length)];
