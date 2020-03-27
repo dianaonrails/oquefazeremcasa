783633332e32ec76d0d609b36b13ab86b46eb906
dianaonrails <d.r.carvalho89@gmail.com>
2020-03-27 14:04:36 +0000

    improvs

--- javascript
+++
@@ -1,6 +0,0 @@
-var coisas = Array("song1", "song2", "song3", "song4", "song5", "song6");
-
-function randomCoisas() {
-  var randomSong = song[Math.floor(Math.random() * song.length)];
-  document.getElementById('randomSong').value = randomSong;
-}
--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-var coisas = Array("Ler um livro", "Pintar", "Videojogos", "Arrumar o armário", "Limpar a casa", "Fazer exercício","Fazer um puzzle","Tomar um banho quentinho",
-	"Fazer panquecas","Aprender uma língua nova","Dançar","Ver um filme","Jogos de tabuleiro", "Meditar", "Yoga", "Pilates", "Jogo do Stop", "Quizzes", "Experimentar receita nova", "Reorganizar os móveis", "Bricolage", "Jogar ao macaquinho do chinês", "Fazer pão", "Fazer um bolo", "Desenhar", "Visitar um museu online", "Escrever");
+
 
 function randomCoisas() {
-  var randomCoisas = coisas[Math.floor(Math.random() * coisas.length)];
+  var lis = document.querySelectorAll('.lista > li');
+  var randomCoisas = lis[Math.floor(Math.random() * lis.length)].innerHTML;
+
   document.getElementById('randomCoisas').innerHTML = randomCoisas;
+
+
+
 }
